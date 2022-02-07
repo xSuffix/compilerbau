c262fb6c4c2f63a772e435fe5545e116c08ff7b8
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2022-02-07 22:39:58 +0100

    Add visitor classes

---
+++ src/visitor/FollowPosTableGenerator.java
@@ -0,0 +1,24 @@
+package visitor;
+
+import c4_1_syntax_tree.BinOpNode;
+import c4_1_syntax_tree.OperandNode;
+import c4_1_syntax_tree.UnaryOpNode;
+import c4_1_syntax_tree.Visitor;
+
+public class FollowPosTableGenerator implements Visitor {
+
+    @Override
+    public void visit(OperandNode node) {
+
+    }
+
+    @Override
+    public void visit(BinOpNode node) {
+
+    }
+
+    @Override
+    public void visit(UnaryOpNode node) {
+
+    }
+}
