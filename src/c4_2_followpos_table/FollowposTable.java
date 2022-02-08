3209540f29f27bf7034ab1eac19ff914ef254c30
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2022-02-08 18:07:05 +0100

    Delete FollowposTable.java

--- src/c4_2_followpos_table/FollowposTable.java
+++
@@ -1,9 +0,0 @@
-package c4_2_followpos_table;
-
-import java.util.SortedMap;
-import java.util.TreeMap;
-
-public class FollowposTable {
-    // Klasse wird nicht benötigt, nur:
-    private final SortedMap<Integer, FollowposTableEntry> followposTableEntries = new TreeMap<>();
-}
