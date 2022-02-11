c3ec01a121efdaf9a142d3713170f7452408caac
nillga <71821765+nillga@users.noreply.github.com>
2022-02-11 11:35:02 +0100

    Lexer implemented, just for fun :)

---
+++ src/main/java/c4_4_generic_lexer/Lexer.java
@@ -0,0 +1,43 @@
+package c4_4_generic_lexer;
+
+import c4_3_transition_matrix_dea.DFAState;
+
+import java.util.Map;
+
+public class Lexer
+{
+    private Map<DFAState, Map<String, DFAState>> stateTransitionTable;
+
+    public Lexer(Map<DFAState, Map<String, DFAState>> stateTransitionTable)
+    {
+        this.stateTransitionTable = stateTransitionTable;
+    }
+
+    public boolean match(String word)
+    {
+        DFAState state = startPos();
+        if (state == null) {
+            return false;
+        }
+
+        String[] symbols = word.split("");
+
+        for (String letter : symbols) {
+            if (!stateTransitionTable.get(state).containsKey(letter)) {
+                return false;
+            }
+            state = stateTransitionTable.get(state).get(letter);
+        }
+        return state.isAcceptingState;
+    }
+
+    private DFAState startPos() {
+        for (DFAState state : stateTransitionTable.keySet()) {
+            if (state.index == 1) {
+                return state;
+            }
+        }
+        return null;
+    }
+}
+
