d536fdabdd2e9bc5f4d606fda815083d93ac7fa2
tobias-kohnle <>
2022-02-11 13:19:36 +0100

    Add simple parser tests

---
+++ src/test/java/TestParser.java
@@ -0,0 +1,106 @@
+import c4_1_syntax_tree.*;
+import c4_2_visitor.*;
+import c4_3_transition_matrix_dea.*;
+import c4_4_generic_lexer.*;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestParser {
+    Parser parser;
+
+    @BeforeEach
+    public void init() {
+        parser = new Parser();
+    }
+
+    @Test
+    public void testEmpty() {
+        parser.initialize("");
+        var actual = parser.start();
+        var expected = new OperandNode("");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSimple1() {
+        parser.initialize("a");
+        var actual = parser.start();
+        var expected = new OperandNode("a");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSimple2() {
+        parser.initialize("ab");
+        var actual = parser.start();
+        var expected = new BinOpNode("°", new OperandNode("a"), new OperandNode("b"));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSimple3() {
+        parser.initialize("a|b");
+        var actual = parser.start();
+        var expected = new BinOpNode("|", new OperandNode("a"), new OperandNode("b"));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSimple4() {
+        parser.initialize("(a)");
+        var actual = parser.start();
+        var expected = new OperandNode("a");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSimple5() {
+        parser.initialize("a*");
+        var actual = parser.start();
+        var expected = new UnaryOpNode("*", new OperandNode("a"));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSimple6() {
+        parser.initialize("(a*)?");
+        var actual = parser.start();
+        var expected = new UnaryOpNode("?", new UnaryOpNode("*", new OperandNode("a")));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSimple7() {
+        parser.initialize("a|");
+        var actual = parser.start();
+        var expected = new BinOpNode("|", new OperandNode("a"), new OperandNode(""));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSimple8() {
+        parser.initialize("|b");
+        var actual = parser.start();
+        var expected = new BinOpNode("|", new OperandNode(""), new OperandNode("b"));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSimple9() {
+        parser.initialize("|");
+        var actual = parser.start();
+        var expected = new BinOpNode("|", new OperandNode(""), new OperandNode(""));
+
+        assertEquals(expected, actual);
+    }
+}
